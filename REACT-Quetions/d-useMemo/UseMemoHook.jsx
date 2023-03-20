868067697ab954f208130138df81221e011ff3b8
pravin0428 <mohite46199@gmail.com>
2023-03-20 23:20:53 +0530

    useMemo hook tutorial

--- REACT-Quetions/d-useMemo/UseMemoHook.jsx
+++ REACT-Quetions/d-useMemo/UseMemoHook.jsx
@@ -1,1 +1,10 @@
 //what is the use of useMemo hook and how can we use it.
+
+//why we use it 
+/* we use hook for optimise the performace of our wesite
+-this hook actually stores the data of heavy dom operations
+ and it will send the data when same request is heat again
+ so it reduces the uneccessory re-renders and increse application
+ performance
+   
+*/
